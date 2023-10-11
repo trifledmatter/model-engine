3b0497687616171ec32627222bea3c444f92cc05
Ethan <mocha@gradison.ca>
2023-10-12 00:04:39 +0100

    Upload

---
+++ build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1369
+#define BUILD_COMMIT "b8fe4b5"
+#define BUILD_COMPILER "cc (Debian 10.2.1-6) 10.2.1 20210110"
+#define BUILD_TARGET "aarch64-linux-gnu"
+
+#endif // BUILD_INFO_H
